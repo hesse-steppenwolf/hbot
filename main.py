6e06240d8024c1b734f04504b6f338195ad4b2e6
Ash C <quemexa@gmail.com>
2022-08-01 08:51:17 -0500

    killweathertest

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import discord, random, datetime, requests, math
 from discord.ext import commands, tasks
 
 TOKEN = open('token.txt', 'r').readlines()[0].strip()
-api_key = "0730be024eb4228d7a46736f4f14304b"
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
 TOOLS = open('tools.txt', 'r', encoding='utf-8').readlines()
 VOWELS = open('name_vowels.txt', 'r').readlines()
 NON_VOWELS = open('name_non_vowels.txt', 'r').readlines()
@@ -80,36 +79,6 @@ async def zalgo(ctx, *args):
             output_word += ch 
         output_sentence.append(output_word)
     await ctx.send(' '.join(output_sentence))
-@client.command()
-async def weather(ctx, *, city: str)
-        await channel.send(embed=embed)
-        else:
-        await channel.send("City not found.")
-    city_name = city
-    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-    response = requests.get(complete_url)
-    x = response.json()
-    channel = ctx.message.channel
-        if x["cod"] != "404":
-        async with channel.typing():
-                        y = x["main"]
-            current_temperature = y["temp"]
-            current_temperature_celsiuis = str(round(current_temperature - 273.15))
-            current_pressure = y["pressure"]
-            current_humidity = y["humidity"]
-            z = x["weather"]
-            weather_description = z[0]["description"]
-            weather_description = z[0]["description"]
-            embed = discord.Embed(title=f"Weather in {city_name}",
-                              color=ctx.guild.me.top_role.color,
-                              timestamp=ctx.message.created_at,)
-            embed.add_field(name="Descripition", value=f"**{weather_description}**", inline=False)
-            embed.add_field(name="Temperature(C)", value=f"**{current_temperature_celsiuis}°C**", inline=False)
-            embed.add_field(name="Humidity(%)", value=f"**{current_humidity}%**", inline=False)
-            embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{current_pressure}hPa**", inline=False)
-            embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
-            embed.set_footer(text=f"Requested by {ctx.author.name}")
-
 
 @client.command()
 async def LEAN(ctx):
